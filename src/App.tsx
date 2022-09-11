71c96ce69ea4d10cd91cadd055e4eb7b0dcc41ff
ToruShimizu <nobbyz.t.nn@gmail.com>
2022-09-11 19:35:16 +0900

    feat: トップページを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import Typography from "@suid/material/Typography";
+import { Component, createSignal } from "solid-js";
+import Button from "@suid/material/Button";
+import Container from "@suid/material/Container";
+import styles from "./App.module.css";
+import Stack from "@suid/material/Stack";
+import Grid from "@suid/material/Grid";
+import Box from "@suid/material/Box";
 
 const App: Component = () => {
+  const [value, setValue] = createSignal("");
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <Container sx={{ p: 3 }}>
+      <Grid container justifyContent="center">
+        <Box>
+          <Typography variant="h3" component="h1" gutterBottom>
+            ライムジェネレーター
+          </Typography>
+        </Box>
+
+        <Stack sx={{ alignItems: "center" }} direction="row" spacing={2}>
+          <textarea
+            id="value"
+            aria-label="value"
+            placeholder="母音に変換したい文字を入力してください"
+            rows="10"
+            cols="50"
+            onchange={(event) => {
+              setValue(event.currentTarget.value);
+            }}
+            class={styles.textarea}
+          >
+            {value()}
+          </textarea>
+          <Button variant="contained" size="large">
+            母音に変換する
+          </Button>
+          <textarea
+            id="value"
+            aria-label="value"
+            placeholder="母音に変換後の文字が入ります"
+            rows="10"
+            cols="50"
+            readonly
+            onchange={(event) => {
+              setValue(event.currentTarget.value);
+            }}
+            class={styles.textarea}
+          >
+            {value()}
+          </textarea>
+        </Stack>
+      </Grid>
+    </Container>
   );
 };
 
