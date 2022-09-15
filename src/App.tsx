e7f51b6aed26730107384d1616f94deb10d3c5b1
ToruShimizu <nobbyz.t.nn@gmail.com>
2022-09-15 22:28:06 +0900

    refactor: [wip]textareaとalert以外をhope-uiに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import Typography from "@suid/material/Typography";
 import { Component, createSignal } from "solid-js";
-import Button from "@suid/material/Button";
-import Container from "@suid/material/Container";
 import styles from "./App.module.css";
-import Stack from "@suid/material/Stack";
 import Alert from "@suid/material/Alert";
 import { ERROR_MESSAGE, useHiraganaRepo } from "./repos/hiragana";
 import { useConvertToVowel } from "./modules/useConvertToVowel";
-import useMediaQuery from "@suid/material/useMediaQuery";
 import AlertTitle from "@suid/material/AlertTitle";
+import { Button, Heading, Stack, Container, Center, Box } from "@hope-ui/solid";
 
 const APP_ID = import.meta.env.VITE_APP_ID as string;
 const EMPTY_VALUE = "母音に変換したい文字を入力してください。";
 
 const { convertToVowel } = useConvertToVowel();
-
-const isSP = useMediaQuery("(max-width: 767px)");
 
 const App: Component = () => {
   const [value, setValue] = createSignal("");
@@ -55,42 +49,45 @@ const App: Component = () => {
   };
 
   return (
-    <Container maxWidth="md" sx={{ textAlign: "center" }}>
-      <Typography variant="h3" component="h1" gutterBottom sx={{ mt: 4 }}>
-        母音変換機
-      </Typography>
-
-      <Stack
-        sx={{ alignItems: "center", justifyContent: "center" }}
-        direction={isSP() ? "column" : "row"}
-        spacing={4}
-      >
-        <textarea
-          id="value"
-          aria-label="value"
-          placeholder="母音に変換したい文字を入力してください"
-          rows="10"
-          cols="50"
-          onInput={(event) => {
-            setValue(event.currentTarget.value);
-          }}
-          class={styles.textarea}
+    <Container maxWidth="md" p={24}>
+      <Center>
+        <Heading level="1" size="4xl" my={24}>
+          母音変換機
+        </Heading>
+      </Center>
+      <Center>
+        <Stack
+          spacing={16}
+          direction={{ "@initial": "column", "@sm": "row" }}
+          mb={36}
         >
-          {value()}
-        </textarea>
+          <textarea
+            id="value"
+            aria-label="value"
+            placeholder="母音に変換したい文字を入力してください"
+            rows="10"
+            cols="50"
+            onInput={(event) => {
+              setValue(event.currentTarget.value);
+            }}
+            class={styles.textarea}
+          >
+            {value()}
+          </textarea>
 
-        <textarea
-          id="value"
-          aria-label="value"
-          placeholder="母音に変換後の文字が入ります"
-          rows="10"
-          cols="50"
-          readonly
-          class={styles.textarea}
-        >
-          {convertedValue()}
-        </textarea>
-      </Stack>
+          <textarea
+            id="value"
+            aria-label="value"
+            placeholder="母音に変換後の文字が入ります"
+            rows="10"
+            cols="50"
+            readonly
+            class={styles.textarea}
+          >
+            {convertedValue()}
+          </textarea>
+        </Stack>
+      </Center>
       {errorMessage() && (
         <Alert severity="error" sx={{ mt: 2 }}>
           <AlertTitle
@@ -102,14 +99,12 @@ const App: Component = () => {
           </AlertTitle>
         </Alert>
       )}
-      <Button
-        sx={{ width: isSP() ? "100%" : 200, mt: 4 }}
-        variant="contained"
-        size="large"
-        onclick={() => convertHiragana(value())}
-      >
-        母音に変換する
-      </Button>
+
+      <Center>
+        <Button onclick={() => convertHiragana(value())} size="lg">
+          母音に変換する
+        </Button>
+      </Center>
     </Container>
   );
 };
