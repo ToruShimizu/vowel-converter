3fffba52003a4a4eaad1b4ab3e9cd4c7f66ee9bc
ToruShimizu <nobbyz.t.nn@gmail.com>
2022-09-15 22:28:01 +0900

    chore: hope-uiをインストール

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { HopeProvider } from "@hope-ui/solid";
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+render(
+  () => (
+    <HopeProvider>
+      <App />
+    </HopeProvider>
+  ),
+  document.getElementById("root") as HTMLElement
+);
